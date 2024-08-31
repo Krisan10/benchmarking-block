13df0979547d6401a680782c762a019972f919c8
Krisan Manoharan <krisan1010@hotmail.com>
2024-08-31 14:30:47 -0400

    Add benchmark

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,7 +1,12 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  yield # This will execute the block of code passed to the method.
+  end_time = Time.now
+  
+  running_time = end_time - start_time
+  running_time
 end
- 
+
 # Be careful, pasting this into IRB will take a long time to print.
 # It's a loooong string. :)
 long_string = "apple"*100000000
